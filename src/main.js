350c248eac0f4721b297df024850658b52973ead
JD Brennan <jazzdev@gmail.com>
2016-05-31 23:49:13 -0700

    Implement load()
    Implemented the load() method to find all the Chrome extensions.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
-exports.load = function () {
+var os = require('os')
+var fs = require('fs')
+var async = require('async')
 
+exports.defaultPath = os.type() === 'Darwin'
+  ? process.env.HOME + '/Library/Application Support/Google/Chrome/Default/Extensions'
+  : (os.type() === 'Windows_NT'
+     ? process.env.USER_PROFILE + '/AppData/Local/Google/Chrome/User Data/Default'
+     : process.env.HOME + '/.config/google-chrome/Default/Extensions/')
+
+exports.load = function (path, callback) {
+  fs.readdir(path, (err, extensionIds) => {
+    if (err) return callback(err)
+    async.map(extensionIds, (id, cb) => loadVersions(path + '/' + id, cb), (err, extensions) => {
+      callback(err, [].concat.apply([], extensions))
+    })
+  })
+}
+
+function loadVersions (path, callback) {
+  fs.readdir(path, (err, extensionVersions) => {
+    if (err) return callback(null, { versions: [] })
+    async.map(extensionVersions, (version, cb) => loadExtension(path + '/' + version, cb), callback)
+  })
+}
+
+function loadExtension (path, callback) {
+  fs.readFile(path + '/manifest.json', (err, contents) => {
+    if (err) return callback(null, { name: err.name, description: err.message })
+    var manifest = JSON.parse(contents)
+    callback(null, {
+      path: path,
+      name: manifest.name,
+      description: manifest.description,
+      version: manifest.version
+    })
+  })
 }
 
 exports.addToWebview = function (webview, extension) {
--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
-var electronChromeExtensions = require('../src/main')
+var chromeExtensions = require('../src/main')
 
 exports.testExport = function(test) {
-	test.expect(2)
-  test.ok(typeof(electronChromeExtensions.load) === 'function', 'load() not exported')
-  test.ok(typeof(electronChromeExtensions.addToWebview) === 'function', 'addToWebView() not exported')
+  test.expect(2)
+  test.equal(typeof(chromeExtensions.load), 'function', 'load() not exported')
+  test.equal(typeof(chromeExtensions.addToWebview), 'function', 'addToWebView() not exported')
   test.done()
+}
+
+exports.testLoad = function(test) {
+  chromeExtensions.load(chromeExtensions.defaultPath, function(err, extensions) {
+    if (err) throw err
+    test.expect(extensions.size)
+    extensions.forEach(function(extension) {
+      test.equal(typeof(extension.name), 'string', 'Extension does not have a name')
+      console.log('Found extension ' + extension.name)
+    })
+    test.done()
+  })
 }
